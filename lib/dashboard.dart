be0f5eb9f10da4ffb9121233ea2949a6f7a527d7
Daniel Seta <99261658+Danielseta@users.noreply.github.com>
2024-05-20 00:55:10 -0700

    text heading + date

--- lib/dashboard.dart
+++ lib/dashboard.dart
@@ -6,11 +6,12 @@ import 'package:flutter/widgets.dart';
 // ignore: must_be_immutable
 class Dashboard extends StatelessWidget {
   var height, width;
+
   @override
   Widget build(BuildContext context) {
-    //TODO: implement build
     height = MediaQuery.of(context).size.height;
     width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: Container(
         color: Colors.grey[400],
@@ -20,73 +21,58 @@ class Dashboard extends StatelessWidget {
           children: [
             Container(
               decoration: BoxDecoration(
-                
+                // Add decoration if needed
               ),
               height: height * 0.45,
               width: width,
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
+              child: Stack( // Use Stack for positioning
                 children: [
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: 35,
-                    left: 15,
-                    right: 15,
-                  ),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                  InkWell(
-                    onTap: () {},
-                    child: Icon(
-                      Icons.sort,
-                      color: Colors.black,
-                      size: 40,
-                    ),
-                  ),
-                  Container(
-                    height: 65,
+                  // Positioned image at the top right corner
+                  Positioned(
+                    top: 15, // Adjust positioning as needed
+                    right: 15, // Adjust positioning as needed
+                    child: Container(
+                      height: 65,
                       width: 65,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(35),
-                      color: Colors.black,
-                      image: DecorationImage(
-                        image: AssetImage("images/Photoroom-20240517_215231.png") 
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(35),
+                        color: Colors.black,
+                        image: DecorationImage(
+                          image: AssetImage("images/Photoroom-20240517_215231.png"),
+                        ),
                       ),
                     ),
-                   
-                    ),
-                
-                    
-                                         ],
-                                ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(
-                    top: 20,
-                    left: 15,
-                    right: 15,
                   ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        "Good moorning Daniel!",
-                        style: TextStyle(
-                          fontSize: 30,
-                          color: Colors.black,
-                          fontWeight: FontWeight.w500,
-                          letterSpacing: 1,
+                  // Greeting and clock text with spacing
+                  Padding(
+                    padding: EdgeInsets.only(top: 35, left: 15, right: 15),
+                    z-index: 1, // Added z-index for better visibility
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        RichText(
+                          text: TextSpan(
+                            style: DefaultTextStyle.of(context).style, // Ensure no transparent color
+                            children: <TextSpan>[
+                              TextSpan(text: 'Good Morning\n', style: TextStyle(fontSize: 30, fontWeight: FontWeight.w500)),
+                              TextSpan(text: 'Daniel', style: TextStyle(fontSize: 24, color: Colors.black)),
+                            ],
+                          ),
                         ),
-                      )
-                    ],
+                        SizedBox(height: 10), // Add spacing between texts
+                        Text(
+                          DateTime.now().toString().split(' ')[0].split('-')[2] +
+                              "." +
+                              DateTime.now().toString().split(' ')[0].split('-')[1],
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ],
+                    ),
                   ),
-               )
-                
-              ],
+                ],
               ),
             ),
-                Container(
+            Container(
               decoration: BoxDecoration(
                 color: Colors.white,
                 borderRadius: BorderRadius.only(
@@ -101,8 +87,5 @@ class Dashboard extends StatelessWidget {
         ),
       ),
     );
-    
   }
-  
 }
-
